a1825dbfc9471361ccd1d71fd616f42b6ead51d3
democ98 <1239865849@qq.com>
2025-07-31 18:01:18 +0800

    feat:add github workflow

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,9 @@ import { nodePolyfills } from "@bangjelkoski/vite-plugin-node-polyfills";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), nodePolyfills({ protocolImports: true })],
+  base: process.env.NODE_ENV === 'production' ? '/injective-react-sc-counter/' : '/',
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+  },
 });
